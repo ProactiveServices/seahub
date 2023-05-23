9546f3da74ab166a6262f18d489a6e517f094a4d
skywalker <dzmbbs@qq.com>
2023-05-23 17:41:27 +0800

    copy_file_from_history

--- seahub/seadoc/urls.py
+++ seahub/seadoc/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .apis import SeadocAccessToken, SeadocUploadLink, SeadocDownloadLink, SeadocUploadFile, \
-    SeadocUploadImage, SeadocDownloadImage
+    SeadocUploadImage, SeadocDownloadImage, SeadocCopyHistoryFile
 
 urlpatterns = [
     url(r'^access-token/(?P<repo_id>[-0-9a-f]{36})/$', SeadocAccessToken.as_view(), name='seadoc_access_token'),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^download-link/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocDownloadLink.as_view(), name='seadoc_download_link'),
     url(r'^upload-image/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocUploadImage.as_view(), name='seadoc_upload_image'),
     url(r'^download-image/(?P<file_uuid>[-0-9a-f]{36})/(?P<filename>.*)$', SeadocDownloadImage.as_view(), name='seadoc_download_image'),
+    url(r'^copy-history-file/(?P<repo_id>[-0-9a-f]{36})/$', SeadocCopyHistoryFile.as_view(), name='seadoc_copy_history_file'),
 ]
