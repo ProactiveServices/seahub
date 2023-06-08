2e262733817a7d9440955e514225724da23efd40
skywalker <dzmbbs@qq.com>
2023-06-08 17:35:14 +0800

    renameSdocHistory

--- seahub/seadoc/urls.py
+++ seahub/seadoc/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .apis import SeadocAccessToken, SeadocUploadLink, SeadocDownloadLink, SeadocUploadFile, \
-    SeadocUploadImage, SeadocDownloadImage
+    SeadocUploadImage, SeadocDownloadImage, SeadocHistory
 
 urlpatterns = [
     url(r'^access-token/(?P<repo_id>[-0-9a-f]{36})/$', SeadocAccessToken.as_view(), name='seadoc_access_token'),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^download-link/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocDownloadLink.as_view(), name='seadoc_download_link'),
     url(r'^upload-image/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocUploadImage.as_view(), name='seadoc_upload_image'),
     url(r'^download-image/(?P<file_uuid>[-0-9a-f]{36})/(?P<filename>.*)$', SeadocDownloadImage.as_view(), name='seadoc_download_image'),
+    url(r'^history/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocHistory.as_view(), name='seadoc_history'),
 ]
